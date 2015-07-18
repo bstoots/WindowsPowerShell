e6ce7459d1d1bd803df57163607fdf159819c3c4
Brian Stoots <bstoots@thewildstoat.com>
2015-07-18 02:04:52 -0400

    Import posh-git module into global session state

--- PSBabushkaDeps/PoshGit-Installed.Dep.ps1
+++ PSBabushkaDeps/PoshGit-Installed.Dep.ps1
@@ -1,10 +1,10 @@
 New-PSBabushkaDep `
 	-Name 'PoshGit-Installed' `
 	-Requires 'PsGet-Installed' `
-	-Met { Get-Command -Module 'Posh-Git' } `
+	-Met { Get-Command -Module 'posh-git' } `
 	-Meet { 
     # We clone manually to avoid having the PsGet installer trash our profile with its defaults
     # TODO: Would be good to get this fixed upstream ...
     git clone "git@github.com:dahlbyk/posh-git.git" "$PSScriptRoot\..\Modules\posh-git" 
-    Import-Module Posh-Git 
+    Import-Module -Global 'posh-git'
   }
