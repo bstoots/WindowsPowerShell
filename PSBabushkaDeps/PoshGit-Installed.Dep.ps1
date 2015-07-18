823c14d121b7bbbc3cb20b7e51adfd14b0331c6d
Brian Stoots <bstoots@thewildstoat.com>
2015-07-18 01:44:33 -0400

    Clone posh-git directly from Github instead of PsGet to avoid default installer adding cruft

--- PSBabushkaDeps/PoshGit-Installed.Dep.ps1
+++ PSBabushkaDeps/PoshGit-Installed.Dep.ps1
@@ -2,4 +2,9 @@ New-PSBabushkaDep `
 	-Name 'PoshGit-Installed' `
 	-Requires 'PsGet-Installed' `
 	-Met { Get-Command -Module 'Posh-Git' } `
-	-Meet { Install-Module 'Posh-Git' }
+	-Meet { 
+    # We clone manually to avoid having the PsGet installer trash our profile with its defaults
+    # TODO: Would be good to get this fixed upstream ...
+    git clone "git@github.com:dahlbyk/posh-git.git" "$PSScriptRoot\..\Modules\posh-git" 
+    Import-Module Posh-Git 
+  }
